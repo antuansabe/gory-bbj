c62d77cd58f24eadaa4841706688587f0f040d8d
Antonio DroCode <antuansabe@gmail.com>
2022-02-20 12:18:16 -0600

    agregando los componentes que conformaran el sitio

--- gory/src/app/app.module.ts
+++ gory/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { InformationSectionComponent } from './componentes/information-section/information-section.component';
+import { GalerySectionComponent } from './componentes/galery-section/galery-section.component';
+import { PlanesPreciosComponent } from './componentes/planes-precios/planes-precios.component';
+import { ProfesoresComponent } from './componentes/profesores/profesores.component';
+import { ContactanosComponent } from './componentes/contactanos/contactanos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    InformationSectionComponent,
+    GalerySectionComponent,
+    PlanesPreciosComponent,
+    ProfesoresComponent,
+    ContactanosComponent
   ],
   imports: [
     BrowserModule,
---
+++ gory/src/app/componentes/planes-precios/planes-precios.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-planes-precios',
+  templateUrl: './planes-precios.component.html',
+  styleUrls: ['./planes-precios.component.scss']
+})
+export class PlanesPreciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
